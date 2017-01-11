83abb550976b05eae8de3561ed419944e69394a4
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-11 21:34:38 +1030

    Updated MultipleChoiceAnswerGroupdTestHarness
    - Implemented superclass abstract methods
    - Updated to use generics

--- library tests/src/main/java/com/matthewtamlin/multiplechoiceanswerview/library_tests/MultipleChoiceAnswerGroupTestHarness.java
+++ library tests/src/main/java/com/matthewtamlin/multiplechoiceanswerview/library_tests/MultipleChoiceAnswerGroupTestHarness.java
@@ -1,10 +1,33 @@
 package com.matthewtamlin.multiplechoiceanswerview.library_tests;
 
+import com.matthewtamlin.multiple_choice_answer_view.library.answer.ImmutableAnswer;
 import com.matthewtamlin.multiple_choice_answer_view.library.answer_group.AnswerGroup;
 import com.matthewtamlin.multiple_choice_answer_view.library.answer_group.MultipleChoiceAnswerGroup;
+import com.matthewtamlin.multiple_choice_answer_view.library.answer_view.DecoratedAnswerCard;
 
-public class MultipleChoiceAnswerGroupTestHarness extends AnswerGroupTestHarness {
+import java.util.Random;
+
+public class MultipleChoiceAnswerGroupTestHarness extends
+		AnswerGroupTestHarness<MultipleChoiceAnswerGroup<DecoratedAnswerCard>> {
 	private MultipleChoiceAnswerGroup testView;
+
+	@Override
+	public MultipleChoiceAnswerGroup<DecoratedAnswerCard> getAnswerView() {
+		final DecoratedAnswerCard answerCard = new DecoratedAnswerCard(this);
+
+		answerCard.addDecorator(colorFadeDecorator, false);
+		answerCard.addDecorator(alphaDecorator, false);
+
+		if ((new Random()).nextBoolean()) {
+			answerCard.setAnswer(new ImmutableAnswer("Correct", true), false);
+		} else {
+			answerCard.setAnswer(new ImmutableAnswer("Incorrect", false), false);
+		}
+
+		answerCard.setStatus(false, false, false);
+
+		return answerCard;
+	}
 
 	@Override
 	public AnswerGroup getTestView() {
