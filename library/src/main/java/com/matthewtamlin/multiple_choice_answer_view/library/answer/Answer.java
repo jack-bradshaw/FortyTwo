cbb9e3fa665e26c79fea34be35bfe319046f99ff
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-14 16:14:33 +1030

    Changed Javadoc

--- library/src/main/java/com/matthewtamlin/multiple_choice_answer_view/library/answer/Answer.java
+++ library/src/main/java/com/matthewtamlin/multiple_choice_answer_view/library/answer/Answer.java
@@ -1,7 +1,7 @@
 package com.matthewtamlin.multiple_choice_answer_view.library.answer;
 
 /**
- * An answer to a question. The answer is made of two parts: <ul><li>The actual text of
+ * An answer to a question, composed of two parts: <ul><li>The actual text of
  * the answer.</li><li>Whether or not the answer is correct.</li></ul>
  */
 public interface Answer {
