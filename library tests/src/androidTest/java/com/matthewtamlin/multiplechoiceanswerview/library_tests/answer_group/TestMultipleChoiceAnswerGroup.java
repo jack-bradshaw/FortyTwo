1d55ebba695cea771db4be8df31ddcaaa322bc86
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-14 15:20:05 +1030

    Expanded test coverage

--- library tests/src/androidTest/java/com/matthewtamlin/multiplechoiceanswerview/library_tests/answer_group/TestMultipleChoiceAnswerGroup.java
+++ library tests/src/androidTest/java/com/matthewtamlin/multiplechoiceanswerview/library_tests/answer_group/TestMultipleChoiceAnswerGroup.java
@@ -7,6 +7,8 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.matthewtamlin.android_testing_tools.library.EspressoHelper;
+import com.matthewtamlin.multiple_choice_answer_view.library.answer_group.AnswerGroup;
+import com.matthewtamlin.multiple_choice_answer_view.library.answer_group.AnswerGroup.Listener;
 import com.matthewtamlin.multiple_choice_answer_view.library.answer_group.MultipleChoiceAnswerGroup;
 import com.matthewtamlin.multiple_choice_answer_view.library.answer_view.AnswerView;
 import com.matthewtamlin.multiple_choice_answer_view.library.answer_view.DecoratedAnswerCard;
@@ -22,11 +24,19 @@ import java.util.List;
 
 import static com.matthewtamlin.multiplechoiceanswerview.library_tests.answer_group.MultipleChoiceAnswerGroupViewActions.addAnswer;
 import static com.matthewtamlin.multiplechoiceanswerview.library_tests.answer_group.MultipleChoiceAnswerGroupViewActions.addAnswers;
+import static com.matthewtamlin.multiplechoiceanswerview.library_tests.answer_group.MultipleChoiceAnswerGroupViewActions.allowSelectionChangesWhenMarked;
 import static com.matthewtamlin.multiplechoiceanswerview.library_tests.answer_group.MultipleChoiceAnswerGroupViewActions.clearAnswers;
+import static com.matthewtamlin.multiplechoiceanswerview.library_tests.answer_group.MultipleChoiceAnswerGroupViewActions.clickViewAtIndex;
+import static com.matthewtamlin.multiplechoiceanswerview.library_tests.answer_group.MultipleChoiceAnswerGroupViewActions.registerListener;
 import static com.matthewtamlin.multiplechoiceanswerview.library_tests.answer_group.MultipleChoiceAnswerGroupViewActions.removeAnswer;
+import static com.matthewtamlin.multiplechoiceanswerview.library_tests.answer_group.MultipleChoiceAnswerGroupViewActions.setMultipleSelectionLimit;
+import static com.matthewtamlin.multiplechoiceanswerview.library_tests.answer_group.MultipleChoiceAnswerGroupViewAssertions.containsNoAnswers;
 import static com.matthewtamlin.multiplechoiceanswerview.library_tests.answer_group.MultipleChoiceAnswerGroupViewAssertions.containsView;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @RunWith(AndroidJUnit4.class)
 public class TestMultipleChoiceAnswerGroup {
@@ -38,10 +48,21 @@ public class TestMultipleChoiceAnswerGroup {
 
 	public ViewInteraction testViewEspresso;
 
+	private Listener listener1;
+
+	private Listener listener2;
+
 	@Before
 	public void setup() {
 		testViewDirect = rule.getActivity().getTestView();
 		testViewEspresso = EspressoHelper.viewToViewInteraction(testViewDirect);
+
+		listener1 = mock(Listener.class);
+		listener2 = mock(Listener.class);
+
+		testViewEspresso.perform(registerListener(listener1));
+		testViewEspresso.perform(registerListener(listener2));
+		testViewEspresso.perform(registerListener(null)); // Check null safety
 	}
 
 	@Test(expected = IllegalArgumentException.class)
@@ -64,6 +85,8 @@ public class TestMultipleChoiceAnswerGroup {
 
 		testViewEspresso.perform(addAnswers(answers));
 
+		testViewEspresso.check(containsNoAnswers());
+
 		assertThat("Expected list to reflect there being no answer cards.",
 				testViewDirect.getAnswers().isEmpty(), is(true));
 	}
@@ -79,6 +102,9 @@ public class TestMultipleChoiceAnswerGroup {
 		for (final DecoratedAnswerCard card : answers) {
 			testViewEspresso.check(containsView(card, true));
 		}
+
+		assertThat("Expected list to contain only the supplied answers.",
+				testViewDirect.getAnswers(), is(answers));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
@@ -93,6 +119,11 @@ public class TestMultipleChoiceAnswerGroup {
 		testViewEspresso.perform(addAnswer(view));
 
 		testViewEspresso.check(containsView(view, true));
+
+		assertThat("Expected list to contain only only one item.",
+				testViewDirect.getAnswers().size(), is(1));
+		assertThat("Expected list to contain only the supplied answer.",
+				testViewDirect.getAnswers().get(0), is(view));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
@@ -105,8 +136,6 @@ public class TestMultipleChoiceAnswerGroup {
 		final AnswerView view = getNewAnswerCard();
 
 		testViewEspresso.perform(removeAnswer(view));
-
-		testViewEspresso.check(containsView(view, false));
 	}
 
 	@Test
@@ -120,6 +149,9 @@ public class TestMultipleChoiceAnswerGroup {
 		testViewEspresso.perform(removeAnswer(view));
 
 		testViewEspresso.check(containsView(view, false));
+
+		assertThat("Expected list to contain no items.", testViewDirect.getAnswers().isEmpty(),
+				is(true));
 	}
 
 	@Test
@@ -139,36 +171,176 @@ public class TestMultipleChoiceAnswerGroup {
 		for (final DecoratedAnswerCard view : answers) {
 			testViewEspresso.check(containsView(view, false));
 		}
+
+		assertThat("Expected list to contain no items.", testViewDirect.getAnswers().isEmpty(),
+				is(true));
 	}
 
-	public void testAnswerViewClickedBehaviour_selectionChangesAllowedViewIsMarked() {
+	@Test
+	public void testClickAnswer_selectionChangesAllowedAndMarked() {
+		final List<DecoratedAnswerCard> answers = new ArrayList<>();
+		answers.add(getNewAnswerCard());
+		answers.add(getNewAnswerCard());
+
+		answers.get(0).setStatus(true, false, false);
+		answers.get(1).setStatus(false, false, false);
+
+		testViewEspresso.perform(addAnswers(answers));
+		testViewEspresso.perform(allowSelectionChangesWhenMarked(true));
+		testViewEspresso.perform(clickViewAtIndex(0));
+
+		assertThat("Answer 0 should be selected.", answers.get(0).isSelected(), is(true));
+		assertThat("Answer 1 should not be selected.", answers.get(1).isSelected(), is(false));
+	}
+
+	@Test
+	public void testClickAnswer_selectionChangesAllowedAndUnmarked() {
+		final List<DecoratedAnswerCard> answers = new ArrayList<>();
+		answers.add(getNewAnswerCard());
+		answers.add(getNewAnswerCard());
+
+		answers.get(0).setStatus(false, false, false);
+		answers.get(1).setStatus(false, false, false);
+
+		testViewEspresso.perform(addAnswers(answers));
+		testViewEspresso.perform(allowSelectionChangesWhenMarked(true));
+		testViewEspresso.perform(clickViewAtIndex(0));
+
+		assertThat("Answer 0 should be selected.", answers.get(0).isSelected(), is(true));
+		assertThat("Answer 1 should not be selected.", answers.get(1).isSelected(), is(false));
+	}
+
+	@Test
+	public void testClickAnswer_selectionChangesDisallowedAndMarked() {
+		final List<DecoratedAnswerCard> answers = new ArrayList<>();
+		answers.add(getNewAnswerCard());
+		answers.add(getNewAnswerCard());
+
+		answers.get(0).setStatus(true, false, false);
+		answers.get(1).setStatus(false, false, false);
+
+		testViewEspresso.perform(addAnswers(answers));
+		testViewEspresso.perform(allowSelectionChangesWhenMarked(false));
+		testViewEspresso.perform(clickViewAtIndex(0));
+
+		assertThat("Answer 0 should not be selected.", answers.get(0).isSelected(), is(false));
+		assertThat("Answer 1 should not be selected.", answers.get(1).isSelected(), is(false));
+	}
+
+	@Test
+	public void testClickAnswer_selectionChangesDisallowedAnUnmarked() {
+		final List<DecoratedAnswerCard> answers = new ArrayList<>();
+		answers.add(getNewAnswerCard());
+		answers.add(getNewAnswerCard());
+
+		answers.get(0).setStatus(false, false, false);
+		answers.get(1).setStatus(false, false, false);
+
+		testViewEspresso.perform(addAnswers(answers));
+		testViewEspresso.perform(allowSelectionChangesWhenMarked(false));
+		testViewEspresso.perform(clickViewAtIndex(0));
+
+		assertThat("Answer 0 should be selected.", answers.get(0).isSelected(), is(true));
+		assertThat("Answer 1 should not be selected.", answers.get(1).isSelected(), is(false));
+	}
+
+	@Test
+	public void testSelectAnswer_selectionCapacityReached() {
+		final List<DecoratedAnswerCard> answers = new ArrayList<>();
+		answers.add(getNewAnswerCard());
+		answers.add(getNewAnswerCard());
+		answers.add(getNewAnswerCard());
+
+		testViewEspresso.perform(addAnswers(answers));
+		testViewEspresso.perform(allowSelectionChangesWhenMarked(true));
+		testViewEspresso.perform(setMultipleSelectionLimit(1));
+
+		testViewEspresso.perform(clickViewAtIndex(0));
+
+		assertThat("Answer 0 should be selected.", answers.get(0).isSelected(), is(true));
+		assertThat("Answer 1 should not be selected.", answers.get(1).isSelected(), is(false));
+		assertThat("Answer 2 should not be selected.", answers.get(2).isSelected(), is(false));
+
+		testViewEspresso.perform(clickViewAtIndex(1));
+
+		assertThat("Answer 0 should not be selected.", answers.get(0).isSelected(), is(false));
+		assertThat("Answer 1 should be selected.", answers.get(1).isSelected(), is(true));
+		assertThat("Answer 2 should not be selected.", answers.get(2).isSelected(), is(false));
+
+		testViewEspresso.perform(clickViewAtIndex(2));
+
+		assertThat("Answer 0 should not be selected.", answers.get(0).isSelected(), is(false));
+		assertThat("Answer 1 should not be selected.", answers.get(1).isSelected(), is(false));
+		assertThat("Answer 2 should be selected.", answers.get(2).isSelected(), is(true));
+	}
+
+	public void testSelectAnswer_selectionCapacityNotExceeded() {
+		final List<DecoratedAnswerCard> answers = new ArrayList<>();
+		answers.add(getNewAnswerCard());
+		answers.add(getNewAnswerCard());
+		answers.add(getNewAnswerCard());
+
+		testViewEspresso.perform(addAnswers(answers));
+		testViewEspresso.perform(allowSelectionChangesWhenMarked(true));
+		testViewEspresso.perform(setMultipleSelectionLimit(3));
+
+		testViewEspresso.perform(clickViewAtIndex(0));
+
+		assertThat("Answer 0 should be selected.", answers.get(0).isSelected(), is(true));
+		assertThat("Answer 1 should not be selected.", answers.get(1).isSelected(), is(false));
+		assertThat("Answer 2 should not be selected.", answers.get(2).isSelected(), is(false));
+
+		testViewEspresso.perform(clickViewAtIndex(1));
+
+		assertThat("Answer 0 should be selected.", answers.get(0).isSelected(), is(true));
+		assertThat("Answer 1 should be selected.", answers.get(1).isSelected(), is(true));
+		assertThat("Answer 2 should not be selected.", answers.get(2).isSelected(), is(false));
+
+		testViewEspresso.perform(clickViewAtIndex(2));
+
+		assertThat("Answer 0 should be selected.", answers.get(0).isSelected(), is(true));
+		assertThat("Answer 1 should be selected.", answers.get(1).isSelected(), is(true));
+		assertThat("Answer 2 should be selected.", answers.get(2).isSelected(), is(true));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetSelectionLimit_limitIsNegative() {
+		testViewEspresso.perform(setMultipleSelectionLimit(-1));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetSelectionLimit_limitIsZero() {
+		testViewEspresso.perform(setMultipleSelectionLimit(0));
+	}
+
+	public void testSetSelectionLimit_limitExceedsCurrentSelectionCount() {
+		final List<DecoratedAnswerCard> answers = new ArrayList<>();
+		answers.add(getNewAnswerCard());
+		answers.add(getNewAnswerCard());
+		answers.add(getNewAnswerCard());
+
+		testViewEspresso.perform(addAnswers(answers));
+		testViewEspresso.perform(allowSelectionChangesWhenMarked(true));
+		testViewEspresso.perform(setMultipleSelectionLimit(3));
+
+		testViewEspresso.perform(clickViewAtIndex(0));
+		testViewEspresso.perform(clickViewAtIndex(1));
+		testViewEspresso.perform(clickViewAtIndex(2));
+
+		testViewEspresso.perform(setMultipleSelectionLimit(1));
+
+		assertThat("Answer 0 should not be selected.", answers.get(0).isSelected(), is(false));
+		assertThat("Answer 1 should not be selected.", answers.get(1).isSelected(), is(false));
+		assertThat("Answer 2 should be selected.", answers.get(2).isSelected(), is(true));
+	}
+
+	public void testSetSelectionLimit_limitDoesNotExceedCurrentSelectionCount() {
 
 	}
 
-	public void testAnswerViewClickedBehaviour_selectionChangesAllowedViewIsNotUnmarked() {
-
-	}
-
-	public void testAnswerViewClickedBehaviour_selectionChangesDisallowedViewIsMarked() {
-
-	}
-
-	public void testAnswerViewClickedBehaviour_selectionChangesDisallowedViewIsUnmarked() {
-
-	}
-
-	public void testAnswerViewClickedBehaviour_selectionLimitNotReached() {
-
-	}
-
-	public void testAnswerViewClickedBehaviour_selectionLimitReached() {
-
-	}
-
-	public void testAnswerViewClickedBehaviour_selectionLimitReachedThenSelectedViewRemoved() {
-
-	}
-
+	/**
+	 * @return a new answer card which is neither selected nor marked
+	 */
 	private DecoratedAnswerCard getNewAnswerCard() {
 		final Context context = InstrumentationRegistry.getTargetContext();
 		return new DecoratedAnswerCard(context);
