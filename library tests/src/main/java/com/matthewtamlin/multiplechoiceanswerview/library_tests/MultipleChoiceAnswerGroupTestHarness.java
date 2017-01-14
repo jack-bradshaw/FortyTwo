b0cfce0566f99d2991869997f228fbb8f78c5dec
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-14 15:18:30 +1030

    Fixed bug by using more than one decorator
    Using the same decorator for multiple views resulted in only one of the views being decorated

--- library tests/src/main/java/com/matthewtamlin/multiplechoiceanswerview/library_tests/MultipleChoiceAnswerGroupTestHarness.java
+++ library tests/src/main/java/com/matthewtamlin/multiplechoiceanswerview/library_tests/MultipleChoiceAnswerGroupTestHarness.java
@@ -1,46 +1,47 @@
 package com.matthewtamlin.multiplechoiceanswerview.library_tests;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.matthewtamlin.multiple_choice_answer_view.library.answer.ImmutableAnswer;
 import com.matthewtamlin.multiple_choice_answer_view.library.answer_group.MultipleChoiceAnswerGroup;
 import com.matthewtamlin.multiple_choice_answer_view.library.answer_view.AlphaDecorator;
 import com.matthewtamlin.multiple_choice_answer_view.library.answer_view.ColorFadeDecorator;
+import com.matthewtamlin.multiple_choice_answer_view.library.answer_view.ColorFadeDecorator.ColorSupplier;
 import com.matthewtamlin.multiple_choice_answer_view.library.answer_view.DecoratedAnswerCard;
 
 import java.util.Random;
 
+@SuppressLint("SetTextI18n") // Not important during testing
 public class MultipleChoiceAnswerGroupTestHarness extends AnswerGroupTestHarness
-				<DecoratedAnswerCard, MultipleChoiceAnswerGroup<DecoratedAnswerCard>> {
+		<DecoratedAnswerCard, MultipleChoiceAnswerGroup<DecoratedAnswerCard>> {
 	/**
 	 * Decorates the test view by changing the background and text colors.
 	 */
-	private final ColorFadeDecorator colorFadeDecorator =
-			new ColorFadeDecorator(new ColorFadeDecorator.ColorSupplier
-					() {
-				@Override
-				public int getColor(final boolean marked, final boolean selected,
-						final boolean answerIsCorrect) {
-					if (marked) {
-						if (selected) {
-							return answerIsCorrect ? Color.GREEN : Color.RED;
-						} else {
-							return answerIsCorrect ? Color.RED : Color.GREEN;
-						}
-					} else {
-						return selected ? Color.BLUE : Color.WHITE;
-					}
+	private final ColorSupplier colorSupplier = new ColorSupplier() {
+		@Override
+		public int getColor(final boolean marked, final boolean selected,
+				final boolean answerIsCorrect) {
+			if (marked) {
+				if (selected) {
+					return answerIsCorrect ? Color.GREEN : Color.RED;
+				} else {
+					return answerIsCorrect ? Color.RED : Color.GREEN;
 				}
-			});
+			} else {
+				return selected ? Color.BLUE : Color.WHITE;
+			}
+		}
+	};
 
 	/**
 	 * Decorates the test view by changing the transparency.
 	 */
-	private final AlphaDecorator
-			alphaDecorator = new AlphaDecorator(new AlphaDecorator.AlphaSupplier() {
+	private final AlphaDecorator.AlphaSupplier
+			alphaSupplier = new AlphaDecorator.AlphaSupplier() {
 		@Override
 		public float getAlpha(final boolean marked, final boolean selected,
 				final boolean answerIsCorrect) {
@@ -50,13 +51,21 @@ public class MultipleChoiceAnswerGroupTestHarness extends AnswerGroupTestHarness
 				return 1f;
 			}
 		}
-	});
+	};
 
 	/**
 	 * The view being tested. The view should be initialised before use so that {@link
 	 * #getTestView()} never returns null.
 	 */
 	private MultipleChoiceAnswerGroup<DecoratedAnswerCard> testView;
+
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		getControlsContainer().addView(createIncreaseMultipleSelectionLimitButton());
+		getControlsContainer().addView(createDecreaseMultipleSelectionLimitButton());
+	}
 
 	@Override
 	public MultipleChoiceAnswerGroup<DecoratedAnswerCard> getTestView() {
@@ -71,8 +80,8 @@ public class MultipleChoiceAnswerGroupTestHarness extends AnswerGroupTestHarness
 	public DecoratedAnswerCard getAnswerView() {
 		final DecoratedAnswerCard answerCard = new DecoratedAnswerCard(this);
 
-		answerCard.addDecorator(colorFadeDecorator, false);
-		answerCard.addDecorator(alphaDecorator, false);
+		answerCard.addDecorator(getNewColorFadeDecorator(), false);
+		answerCard.addDecorator(getNewAlphaDecorator(), false);
 
 		if ((new Random()).nextBoolean()) {
 			answerCard.setAnswer(new ImmutableAnswer("Correct", true), false);
@@ -83,5 +92,45 @@ public class MultipleChoiceAnswerGroupTestHarness extends AnswerGroupTestHarness
 		answerCard.setStatus(false, false, false);
 
 		return answerCard;
+	}
+
+	private Button createIncreaseMultipleSelectionLimitButton() {
+		final Button b = new Button(this);
+		b.setText("Increase multiple selection limit");
+		b.setAllCaps(false);
+
+		b.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(final View v) {
+				final int currentLimit = getTestView().getMultipleSelectionLimit();
+				getTestView().setMultipleSelectionLimit(currentLimit + 1);
+			}
+		});
+
+		return b;
+	}
+
+	private Button createDecreaseMultipleSelectionLimitButton() {
+		final Button b = new Button(this);
+		b.setText("Decrease multiple selection limit");
+		b.setAllCaps(false);
+
+		b.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(final View v) {
+				final int currentLimit = getTestView().getMultipleSelectionLimit();
+				getTestView().setMultipleSelectionLimit(currentLimit - 1);
+			}
+		});
+
+		return b;
+	}
+
+	private ColorFadeDecorator getNewColorFadeDecorator() {
+		return new ColorFadeDecorator(colorSupplier);
+	}
+
+	private AlphaDecorator getNewAlphaDecorator() {
+		return new AlphaDecorator(alphaSupplier);
 	}
 }
