c04ceaf9f8e9b18ba847bccff0fccf31893aa61e
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-14 18:39:28 +1030

    Added and changed Javadoc

--- library/src/main/java/com/matthewtamlin/multiple_choice_answer_view/library/answer_group/AnswerGroup.java
+++ library/src/main/java/com/matthewtamlin/multiple_choice_answer_view/library/answer_group/AnswerGroup.java
@@ -24,38 +24,96 @@ import java.util.List;
  */
 public interface AnswerGroup<V extends AnswerView> extends Listenable<Listener<V>> {
 	/**
-	 * Adds all views in the supplied collection. The order of the collection determines the order
-	 * in which the views are added. The supplied collection must not be null and must not contain
-	 * null.
+	 * Adds a collection of answer views to this group. The order of the collection determines the
+	 * order in which the views are added. The supplied collection must not be null and must not
+	 * contain null.
 	 *
 	 * @param answers
-	 * 		the views to add, not null, not containing null
+	 * 		the answer views to add, not null, not containing null
 	 */
 	public void addAnswers(Collection<V> answers);
 
 	/**
-	 * Adds a single answer to the view. The supplied answer must not be null
+	 * Adds the supplied answer view to this group. The supplied view must not be null.
 	 *
 	 * @param answer
 	 * 		the answer to add, not null
 	 */
 	public void addAnswer(V answer);
 
+	/**
+	 * Removes the supplied answer view from this group. If the supplied answer is not contained in
+	 * the group then the method exists normally.
+	 *
+	 * @param answer
+	 * 		the answer to remove, not null
+	 */
 	public void removeAnswer(V answer);
 
+	/**
+	 * Removes all answers views currently in this group.
+	 */
 	public void clearAnswers();
 
+	/**
+	 * Returns a collection containing all answer views currently in this group. Implementations may
+	 * return an unmodifiable list. The returned list may be empty, but it will never be null.
+	 *
+	 * @return the collection of views
+	 */
 	public List<V> getAnswers();
 
+	/**
+	 * Enables/disables selection changes on all answer views which are marked. Clicks on marked
+	 * views will not change the selection status if this is enabled.
+	 *
+	 * @param allow
+	 * 		whether or not to allow marked views to have selection changes
+	 */
 	public void allowSelectionChangesWhenMarked(boolean allow);
 
+	/**
+	 * @return true if the group is currently set to allow selection changes on marked views, false
+	 * otherwise
+	 */
 	public boolean selectionChangesAreAllowedWhenMarked();
 
+	/**
+	 * Declares to this view that a contained answer view has been selected or deselected
+	 * externally. This method must be called whenever such an event occurs. Calling it when no
+	 * external selection change has occurred has no effect, however excessive calls will limit
+	 * performance.
+	 */
 	public void declareExternalViewSelectionChanges();
 
+	/**
+	 * Callback listener to be called when an AnswerView in an AnswerGroup is selected or
+	 * deselected.
+	 *
+	 * @param <V>
+	 * 		the type of answer views in the AnswerGroup
+	 */
 	public interface Listener<V extends AnswerView> {
+		/**
+		 * Invoked when an AnswerView is selected in the AnswerGroup this listener is registered
+		 * to.
+		 *
+		 * @param answerGroup
+		 * 		the AnswerGroup containing the selected view, not null
+		 * @param selectedView
+		 * 		the view which was selected, not null
+		 */
 		public void onAnswerSelected(final AnswerGroup<V> answerGroup, final V selectedView);
 
+		/**
+		 * Invoked when an AnswerView is deselected in the AnswerGroup this listener is registered
+		 * to.
+		 *
+		 * @param answerGroup
+		 * 		the AnswerGroup containing the deselected view, not null
+		 * @param deselectedView
+		 * 		the view which was deselected, not null
+		 */
 		public void onAnswerDeselected(final AnswerGroup<V> answerGroup, final V deselectedView);
 	}
 }
