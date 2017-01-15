abf4dd6cd0a08475610ebc2b3c4c582fdb0bf9a9
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-15 15:49:51 +1030

    Added Javadoc

--- library tests/src/test/java/com/matthewtamlin/multiplechoiceanswerview/library_tests/answer/TestImmutableAnswer.java
+++ library tests/src/test/java/com/matthewtamlin/multiplechoiceanswerview/library_tests/answer/TestImmutableAnswer.java
@@ -9,13 +9,26 @@ import org.junit.runners.JUnit4;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+/**
+ * Unit tests for the {@link ImmutableAnswer} class.
+ */
 @RunWith(JUnit4.class)
 public class TestImmutableAnswer {
+	/**
+	 * Test to ensure the {@link ImmutableAnswer#ImmutableAnswer(CharSequence, boolean)}
+	 * construction functons correctly when provided with null text. The test will only pass if the
+	 * correct exception is thrown.
+	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testConstructor_nullText() {
 		new ImmutableAnswer(null, true);
 	}
 
+	/**
+	 * Test to ensure the {@link ImmutableAnswer#ImmutableAnswer(CharSequence, boolean)}
+	 * construction functons correctly when provided with non-null text. The test will only pass if
+	 * the getters return the values passed the constructor.
+	 */
 	@Test
 	public void testConstructor_validArguments() {
 		final ImmutableAnswer answer = new ImmutableAnswer("test", true);
@@ -23,4 +36,4 @@ public class TestImmutableAnswer {
 		assertThat("getter returned wrong text.", answer.getText(), is((CharSequence) "test"));
 		assertThat("getter returned wrong correctness", answer.isCorrect(), is(true));
 	}
-}
+}	
