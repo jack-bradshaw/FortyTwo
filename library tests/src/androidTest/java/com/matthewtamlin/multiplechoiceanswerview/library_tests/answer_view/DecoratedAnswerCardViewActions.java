c14f53ecf9a0eda55ad73b458773b84dfbdbeaa3
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-16 22:00:58 +1030

    Revert "Renamed package from multiple_selection_answer_view to response"
    This reverts commit 03bdea41bc311e6b0797dd853d5b102a18508ba9.

---
+++ library tests/src/androidTest/java/com/matthewtamlin/multiplechoiceanswerview/library_tests/answer_view/DecoratedAnswerCardViewActions.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2017 Matthew Tamlin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.matthewtamlin.multiplechoiceanswerview.library_tests.answer_view;
+
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.view.View;
+
+import com.matthewtamlin.multiple_choice_answer_view.library.answer_view.DecoratedAnswerCard;
+import com.matthewtamlin.multiple_choice_answer_view.library.answer_view.DecoratedAnswerCard.Decorator;
+
+import org.hamcrest.Matcher;
+
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+
+/**
+ * Custom Espresso view actions which can be applied to DecoratedAnswerCard views.
+ */
+public class DecoratedAnswerCardViewActions {
+	/**
+	 * The duration to wait for asynchronous events to finish, measured in milliseconds
+	 */
+	private static final int WAIT_FOR_ASYNC_EVENTS_TO_FINISH_MS = 1500;
+
+	/**
+	 * Creates a ViewAction which can be applied to a DecoratedAnswerCard to add a decorator.
+	 *
+	 * @param decorator
+	 * 		the decorator to add
+	 * @param animate
+	 * 		whether or not the initial decoration should be animated
+	 * @return the view action
+	 */
+	public static ViewAction addDecorator(final Decorator decorator, final boolean animate) {
+		return new ViewAction() {
+			@Override
+			public Matcher<View> getConstraints() {
+				return isAssignableFrom(DecoratedAnswerCard.class);
+			}
+
+			@Override
+			public String getDescription() {
+				return "add decorator";
+			}
+
+			@Override
+			public void perform(final UiController uiController, final View view) {
+				((DecoratedAnswerCard) view).addDecorator(decorator, animate);
+
+				if (animate) {
+					uiController.loopMainThreadForAtLeast(WAIT_FOR_ASYNC_EVENTS_TO_FINISH_MS);
+				}
+			}
+		};
+	}
+
+	/**
+	 * Creates a ViewAction which can be applied to a DecoratedAnswerCard to remove a decorator.
+	 *
+	 * @param decorator
+	 * 		the decorator to remove
+	 * @return the view action
+	 */
+	public static ViewAction removeDecorator(final Decorator decorator) {
+		return new ViewAction() {
+			@Override
+			public Matcher<View> getConstraints() {
+				return isAssignableFrom(DecoratedAnswerCard.class);
+			}
+
+			@Override
+			public String getDescription() {
+				return "remove decorator";
+			}
+
+			@Override
+			public void perform(final UiController uiController, final View view) {
+				((DecoratedAnswerCard) view).removeDecorator(decorator);
+			}
+		};
+	}
+}
---
+++ library/src/main/java/com/matthewtamlin/multiple_choice_answer_view/library/answer_view/DecoratedAnswerCard.java
@@ -0,0 +1,194 @@
+/*
+ * Copyright 2017 Matthew Tamlin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.matthewtamlin.multiple_choice_answer_view.library.answer_view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.matthewtamlin.android_utilities.library.testing.Tested;
+import com.matthewtamlin.java_utilities.checkers.IntChecker;
+import com.matthewtamlin.multiple_choice_answer_view.library.answer.Answer;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * An implementation of the AnswerCard interface which can be customised by supplying one or more
+ * decorators. Any decorator supplied to {@link #addDecorator(Decorator, boolean)} will be called
+ * upon whenever the status, answer or identifier changes.
+ */
+@Tested(testMethod = "automated", requiresInstrumentation = true)
+public class DecoratedAnswerCard extends SimpleAnswerCard {
+	/**
+	 * All decorators currently registered with the card.
+	 */
+	private final Set<Decorator> decorators = new HashSet<>();
+
+	/**
+	 * Constructs a new DecoratedAnswerCard. The marked and selected statuses are both set to false
+	 * by default.
+	 *
+	 * @param context
+	 * 		the context this view is operating in, not null
+	 */
+	public DecoratedAnswerCard(final Context context) {
+		super(context);
+	}
+
+	/**
+	 * Constructs a new DecoratedAnswerCard. The marked and selected statuses are both set to false
+	 * by default.
+	 *
+	 * @param context
+	 * 		the context this view is operating in, not null
+	 * @param attrs
+	 * 		configuration attributes, null allowed
+	 */
+	public DecoratedAnswerCard(final Context context, final AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	/**
+	 * Constructs a new DecoratedAnswerCard. The marked and selected statuses are both set to false
+	 * by default.
+	 *
+	 * @param context
+	 * 		the context this view is operating in, not null
+	 * @param attrs
+	 * 		configuration attributes, null allowed
+	 * @param defStyleAttr
+	 * 		an attribute in the current theme which supplies default attributes, pass 0	to ignore
+	 */
+	public DecoratedAnswerCard(final Context context, final AttributeSet attrs,
+			final int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	/**
+	 * Registers a decorator with this view and calls its {@link Decorator#decorate
+	 * (DecoratedAnswerCard, boolean)} method immediately. If the decorator is null or has already
+	 * been added, then there is no effect and the method returns immediately.
+	 *
+	 * @param decorator
+	 * 		the decorator to add
+	 * @param animate
+	 * 		whether or not the initial decoration should be animated
+	 */
+	public void addDecorator(final Decorator decorator, boolean animate) {
+		if (decorator != null) {
+			decorators.add(decorator);
+			decorator.setAnimationDurationMs(getAnimationDurationMs());
+			decorator.decorate(this, animate);
+		}
+	}
+
+	/**
+	 * Removes a decorator from this view. If the decorator is null or has not been added, then
+	 * there is no effect and the method returns immediately. If the decorator has modified the view
+	 * in some way, those modifications are not reversed.
+	 *
+	 * @param decorator
+	 * 		the decorator to remove
+	 */
+	public void removeDecorator(final Decorator decorator) {
+		decorators.remove(decorator);
+	}
+
+	/**
+	 * Removes all decorators from the view. If the decorators have modified the view in some way,
+	 * those modifications are not reversed.
+	 */
+	public void clearDecorators() {
+		decorators.clear();
+	}
+
+	/**
+	 * Returns an unmodifiable set containing all decorators currently registered with this view.
+	 * The set may be empty but it will never be null.
+	 *
+	 * @return the set of all current decorators
+	 */
+	public Set<Decorator> getDecorators() {
+		return Collections.unmodifiableSet(decorators);
+	}
+
+	@Override
+	public void setStatus(final boolean marked, final boolean selected, final boolean animate) {
+		super.setStatus(marked, selected, animate);
+
+		for (final Decorator decorator : decorators) {
+			decorator.decorate(this, animate);
+		}
+	}
+
+	@Override
+	public void setAnswer(final Answer answer, final boolean animate) {
+		super.setAnswer(answer, animate);
+
+		for (final Decorator decorator : decorators) {
+			decorator.decorate(this, animate);
+		}
+	}
+
+	@Override
+	public void setAnimationDurationMs(final int animationDurationMs) {
+		IntChecker.checkGreaterThanOrEqualTo(animationDurationMs, 0, "animationDurationMs cannot " +
+				"be less than zero.");
+
+		super.setAnimationDurationMs(animationDurationMs);
+
+		for (final Decorator decorator : decorators) {
+			decorator.setAnimationDurationMs(animationDurationMs);
+		}
+	}
+
+	/**
+	 * Applies decoration to a single {@link DecoratedAnswerCard} via the {@link
+	 * DecoratedAnswerCard#addDecorator(Decorator, boolean)} method. In general, it is not safe to
+	 * apply a single instance to multiple views.
+	 */
+	public interface Decorator {
+		/**
+		 * Sets the duration to use for any animations this decorator performs when {@link
+		 * #decorate(DecoratedAnswerCard, boolean)} is called.
+		 *
+		 * @param animationDurationMs
+		 * 		the duration to use, measured in milliseconds, greater than zero
+		 */
+		public void setAnimationDurationMs(int animationDurationMs);
+
+		/**
+		 * Returns the duration used for any animations this decorator performs when {@link
+		 * #decorate(DecoratedAnswerCard, boolean)} is called.
+		 *
+		 * @return the current animation duration, measured in milliseconds
+		 */
+		public int getAnimationDurationMs();
+
+		/**
+		 * Applies the decoration to the supplied card. If the decoration is animated, the duration
+		 * last passed to {@link #setAnimationDurationMs(int)} is used.
+		 *
+		 * @param cardToDecorate
+		 * 		the card to apply the decoration to, not null
+		 * @param animate
+		 * 		whether or not decoration animations should be enabled
+		 */
+		public void decorate(DecoratedAnswerCard cardToDecorate, boolean animate);
+	}
+}
