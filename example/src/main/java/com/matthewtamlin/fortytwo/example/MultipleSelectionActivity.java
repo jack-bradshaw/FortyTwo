1ab989a795a42fb230436e758583bdbce55dca65
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-18 17:07:53 +1030

    Created MultipleSelectionActivity.java

---
+++ example/src/main/java/com/matthewtamlin/fortytwo/example/MultipleSelectionActivity.java
@@ -0,0 +1,23 @@
+package com.matthewtamlin.fortytwo.example;
+
+import com.matthewtamlin.fortytwo.library.answer.Answer;
+import com.matthewtamlin.fortytwo.library.answer.ImmutableAnswer;
+
+import java.util.LinkedHashMap;
+
+public class MultipleSelectionActivity {
+	/**
+	 * The question to display.
+	 */
+	private static final String QUESTION = "Which of the following are moons of Jupiter? " +
+			"Select up to four answers.";
+
+	/**
+	 * The answers and the associated identifiers to display.
+	 */
+	private static final LinkedHashMap<CharSequence, Answer> answerMap = new LinkedHashMap<>();
+
+	static {
+
+	}
+}
