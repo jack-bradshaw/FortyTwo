512f545b22cde4da9d5569bfd6f1e38afe678071
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-18 17:23:10 +1030

    Created decorators and added answers to view

--- example/src/main/java/com/matthewtamlin/fortytwo/example/MultipleSelectionActivity.java
+++ example/src/main/java/com/matthewtamlin/fortytwo/example/MultipleSelectionActivity.java
@@ -1,9 +1,17 @@
 package com.matthewtamlin.fortytwo.example;
 
+import android.view.ViewGroup;
+
 import com.matthewtamlin.fortytwo.library.answer.Answer;
 import com.matthewtamlin.fortytwo.library.answer.ImmutableAnswer;
+import com.matthewtamlin.fortytwo.library.answer_view.AlphaDecorator;
+import com.matthewtamlin.fortytwo.library.answer_view.ColorFadeDecorator;
+import com.matthewtamlin.fortytwo.library.answer_view.DecoratedAnswerCard;
 
 import java.util.LinkedHashMap;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 public class MultipleSelectionActivity extends AbstractQuestionActivity{
 	/**
@@ -27,5 +35,71 @@ public class MultipleSelectionActivity extends AbstractQuestionActivity{
 		answerMap.put("", new ImmutableAnswer("Titan", false));
 	}
 
+	@Override
+	protected void onStart() {
+		super.onStart();
+		displayQuestionAndAnswers();
+	}
 
+	/**
+	 * Adds all answer and identifiers to the view.
+	 */
+	@SuppressWarnings("unchecked")
+	private void displayQuestionAndAnswers() {
+		getQuestionContainer().setText(QUESTION);
+
+		for (final CharSequence identifier : answerMap.keySet()) {
+			final DecoratedAnswerCard decoratedAnswerCard = new DecoratedAnswerCard(this);
+
+			decoratedAnswerCard.setLayoutParams(new ViewGroup.LayoutParams(MATCH_PARENT, WRAP_CONTENT));
+			decoratedAnswerCard.setIdentifier(identifier, false);
+			decoratedAnswerCard.setAnswer(answerMap.get(identifier), false);
+			decoratedAnswerCard.addDecorator(createColorFadeDecorator(), false);
+			decoratedAnswerCard.addDecorator(createAlphaDecorator(), false);
+
+			getAnswerGroup().addAnswer(decoratedAnswerCard);
+		}
+	}
+
+	/**
+	 * @return a new ColorFadeDecorator
+	 */
+	private ColorFadeDecorator createColorFadeDecorator() {
+		final ColorFadeDecorator.ColorSupplier colorSupplier = new ColorFadeDecorator.ColorSupplier() {
+			@Override
+			public int getColor(final boolean marked, final boolean selected,
+					final boolean answerIsCorrect) {
+				if (marked) {
+					if (selected) {
+						return answerIsCorrect ? 0xFF2E7D32 : 0xFFb71c1c; // Green, red
+					} else {
+						return answerIsCorrect ? 0xFF673AB7 : 0xFFFFFFFF; // Purple, white
+					}
+				} else {
+					return selected ? 0xFFFF9800 : 0xFFFFFFFF; // Orange, white
+				}
+			}
+		};
+
+		return new ColorFadeDecorator(colorSupplier);
+	}
+
+	/**
+	 * @return a new AlphaDecorator
+	 */
+	private AlphaDecorator createAlphaDecorator() {
+		final AlphaDecorator.AlphaSupplier alphaSupplier = new AlphaDecorator.AlphaSupplier() {
+			@Override
+			public float getAlpha(final boolean marked, final boolean selected,
+					final boolean answerIsCorrect) {
+				if (marked && !selected && !answerIsCorrect) {
+					return 0.3f;
+				} else {
+					return 1f;
+				}
+			}
+		};
+
+		return new AlphaDecorator(alphaSupplier);
+	}
 }
