3b984cc0779a0eb180ed59631f0e1fdb25537f21
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-18 16:47:43 +1030

    Added button on click behaviour

--- example/src/main/java/com/matthewtamlin/fortytwo/example/AbstractQuestionActivity.java
+++ example/src/main/java/com/matthewtamlin/fortytwo/example/AbstractQuestionActivity.java
@@ -3,11 +3,13 @@ package com.matthewtamlin.fortytwo.example;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.matthewtamlin.fortytwo.library.answer_group.AnswerGroup;
 import com.matthewtamlin.fortytwo.library.answer_group.SelectionLimitedAnswerGroup;
+import com.matthewtamlin.fortytwo.library.answer_view.AnswerView;
 
 public class AbstractQuestionActivity extends AppCompatActivity {
 	/**
@@ -38,5 +40,22 @@ public class AbstractQuestionActivity extends AppCompatActivity {
 		questionContainer = (TextView) findViewById(R.id.main_Activity_question_container);
 		answerGroup = (SelectionLimitedAnswerGroup) findViewById(R.id.main_activity_answer_group);
 		actionButton = (Button) findViewById(R.id.main_activity_action_button);
+
+		actionButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(final View v) {
+				for (final AnswerView answerView : answerGroup.getAnswers()) {
+					answerView.setMarkedStatus(!currentlyMarked, true);
+				}
+
+				currentlyMarked = !currentlyMarked;
+
+				if (currentlyMarked) {
+					actionButton.setText("Reset");
+				} else {
+					actionButton.setText("Submit answers");
+				}
+			}
+		});
 	}
 }
