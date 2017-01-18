28ab8d2d4c03dc58101a98cf9723b4d85804d61f
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-18 16:44:03 +1030

    Added member variables for UI elements and state

--- example/src/main/java/com/matthewtamlin/fortytwo/example/AbstractQuestionActivity.java
+++ example/src/main/java/com/matthewtamlin/fortytwo/example/AbstractQuestionActivity.java
@@ -3,11 +3,40 @@ package com.matthewtamlin.fortytwo.example;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.matthewtamlin.fortytwo.library.answer_group.AnswerGroup;
+import com.matthewtamlin.fortytwo.library.answer_group.SelectionLimitedAnswerGroup;
 
 public class AbstractQuestionActivity extends AppCompatActivity {
+	/**
+	 * TextView which contains the question.
+	 */
+	private TextView questionContainer;
+
+	/**
+	 * AnswerGroup which contains the answers.
+	 */
+	private AnswerGroup answerGroup;
+
+	/**
+	 * A button for marking and unmarking the answers.
+	 */
+	private Button actionButton;
+
+	/**
+	 * Whether or not the answers are currently marked.
+	 */
+	private boolean currentlyMarked = false;
+
 	@Override
 	protected void onCreate(@Nullable final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.question_activity);
+
+		questionContainer = (TextView) findViewById(R.id.main_Activity_question_container);
+		answerGroup = (SelectionLimitedAnswerGroup) findViewById(R.id.main_activity_answer_group);
+		actionButton = (Button) findViewById(R.id.main_activity_action_button);
 	}
 }
